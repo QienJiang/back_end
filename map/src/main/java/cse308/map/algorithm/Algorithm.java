1b545cb3a59037517617124e56f82ae081421a9a
linzengxian <linzengxian1997@gmail.com>
2019-04-20 13:48:24 -0700

    change color

--- map/src/main/java/cse308/map/algorithm/Algorithm.java
+++ map/src/main/java/cse308/map/algorithm/Algorithm.java
@@ -1,5 +1,6 @@
 package cse308.map.algorithm;
 
+import com.corundumstudio.socketio.SocketIOClient;
 import cse308.map.model.*;
 import cse308.map.server.PrecinctService;
 
@@ -14,17 +15,21 @@ public class Algorithm {
     private Map<String,Precinct> precincts = new HashMap<>();
     private Map<String, Cluster> clusters = new HashMap<>();
 
+    private SocketIOClient client;
     //pass the precinctService to the algorithm object because we can't autowired precinctService for each object it is not working.
-    public Algorithm(String stateName,int desireDistrict,int numOfRun,PrecinctService precinctService ){
+    public Algorithm(String stateName, int desireDistrict, int numOfRun, PrecinctService precinctService, SocketIOClient client){
         for(int i =0; i < numOfRun;i++){
             states.put(i,new State(i,stateName));
         }
         this.precinctService = precinctService;
+        this.client = client;
     }
     private void init(){
+        String[] colors = {"red","purple","yellow","orange","blue"};
         Iterable<Precinct> allPrecincts = precinctService.getAllPrecincts();
         for(Precinct p : allPrecincts){
             precincts.put(p.getId(),p);
+            client.sendEvent("messageevent", p.getId() + ":" + colors[(int)(Math.random()*colors.length)]);
         }
 
         for(Precinct p : precincts.values()){
